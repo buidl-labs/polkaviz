3298ccdfe9d039dc40e00dcf0deea088870ecf15
Prastut Kumar <kr.prastut@gmail.com>
2019-07-24 13:39:17 +0530

    prettified the code

--- polkaviz-app/src/components/Validator.js
+++ polkaviz-app/src/components/Validator.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Rectangle from "./Rectangle";
-import Tail from "./Tail"
+import Tail from "./Tail";
 
 class Validator extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Tail x={this.props.x / 2} y={this.props.y / 2} angle={this.props.angle} />
-        <Rectangle x={this.props.x / 2} y={this.props.y / 2} angle={this.props.angle} />
+        <Tail
+          x={this.props.x / 2}
+          y={this.props.y / 2}
+          angle={this.props.angle}
+        />
+        <Rectangle
+          x={this.props.x / 2}
+          y={this.props.y / 2}
+          angle={this.props.angle}
+        />
       </React.Fragment>
     );
   }
