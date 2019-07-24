e03ed329a90bf4fe53c23476cb787744422aa681
Prastut Kumar <kr.prastut@gmail.com>
2019-07-24 16:06:32 +0530

    saving progress for block-animation

--- polkaviz-app/src/components/App.js
+++ polkaviz-app/src/components/App.js
@@ -3,6 +3,7 @@ import Relay from "./Relay";
 import { Stage, Layer } from "react-konva";
 import Validator from "./Validator";
 import validatorData from "./data.json";
+import BlockAnimation from "./BlockAnimation";
 
 class App extends React.Component {
   render() {
@@ -34,6 +35,32 @@ class App extends React.Component {
                   y={
                     window.innerHeight +
                     350 *
+                      Math.sin((90 - (index * 360) / arr.length) * 0.0174533)
+                  }
+                />
+              ))}
+              {arr.map((person, index) => (
+                <BlockAnimation
+                  key={index}
+                  angle={180 - (index * 360) / arr.length}
+                  x1={
+                    window.innerWidth/2 +
+                    118 *
+                      Math.cos((90 - (index * 360) / arr.length) * 0.0174533)
+                  }
+                  y1={
+                    window.innerHeight/2 +
+                    118 *
+                      Math.sin((90 - (index * 360) / arr.length) * 0.0174533)
+                  }
+                  x2={
+                    window.innerWidth/2 +
+                    170 *
+                      Math.cos((90 - (index * 360) / arr.length) * 0.0174533)
+                  }
+                  y2={
+                    window.innerHeight/2 +
+                    170 *
                       Math.sin((90 - (index * 360) / arr.length) * 0.0174533)
                   }
                 />
