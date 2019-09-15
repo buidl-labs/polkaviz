5290a860e51155becb3468ee9b1cff5aae804fdc
Prastut Kumar <kr.prastut@gmail.com>
2019-09-15 16:44:59 +0530

    Data fetching speed fixed, error occuring on clicking validators

--- polkaviz-app/src/components/App.js
+++ polkaviz-app/src/components/App.js
@@ -5,7 +5,7 @@ import Validator from "./Validator";
 import BlockAnimation from "./BlockAnimation";
 import { WsProvider, ApiPromise } from "@polkadot/api";
 import Bottombar from "./Bottombar";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 // import Parachains from './Parachains'
 
 class App extends React.Component {
@@ -16,15 +16,15 @@ class App extends React.Component {
       validators: [],
       lastAuthor: "",
       start: null,
-      isloading:true,
-      valtotalinfo:[]
+      isloading: true,
+      valtotalinfo: []
     };
-    this.ismounted = true
+    this.ismounted = true;
   }
   componentDidMount() {
-    console.log(this.props)
-    if(!this.props.data){
-    this.createApi();
+    console.log(this.props);
+    if (!this.props.data) {
+      this.createApi();
     }
   }
   async createApi() {
@@ -33,80 +33,98 @@ class App extends React.Component {
     await api.derive.chain.subscribeNewHead(block => {
       // console.log(`block #${block.author}`);
       const lastAuthor = block.author.toString();
-      if(this.ismounted){
-      this.setState({ lastAuthor });
+      if (this.ismounted) {
+        this.setState({ lastAuthor });
       }
       const start = new Date();
-      if(this.ismounted){
-      this.setState({ start: start });
+      if (this.ismounted) {
+        this.setState({ start: start });
       }
     });
 
     await api.query.session.validators(validators => {
       const sessionValidators = validators.map(x => x.toString());
-      if(this.ismounted){
-      this.setState({ 
-        validators: sessionValidators       
-       });
+      if (this.ismounted) {
+        this.setState({
+          validators: sessionValidators
+        });
       }
     });
 
+    // Retrieve the balances for all validators
 
-
-
-
-    async function asyncForEach(array, callback) {
-      for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-      }
-    }
-    const start = async () => {
-      let arr1 =[]
-      let count =0
-      await asyncForEach(this.state.validators, async (val) => {
-        console.log(val,count++)
-        let stakers = await api.derive.staking.info(val)
-        let stakeinfo = JSON.parse(stakers)
-        console.log(stakeinfo.stakers.others)
-        arr1.push({
-          valname:val,
-          valinfo:stakeinfo
-          })
-        // stakeinfo.stakers.others.forEach(ele => {
-        //   if(ele.who === this.props.match.params.nominatorAddress)
-        //   {
-        //     arr1.push(val)
-        //     bonded += ele.value /Math.pow(10,15)
-        //   }
-        // })
+    const validatorInfo = await Promise.all(
+      this.state.validators.map(authorityId =>
+        api.derive.staking.info(authorityId)
+      )
+    );
+    let arr1 = [];
+    this.state.validators.forEach((authorityId, index) => {
+      console.log("Validator: " + authorityId.toString());
+      // validatorInfo[index] = JSON.parse(validatorInfo[index]);
+      arr1.push({
+        valname: authorityId,
+        valinfo: validatorInfo[index]
       });
-      console.log('Done');
-      console.log(arr1)
-      this.setState({
-        valtotalinfo:arr1,
-        isloading: false
-      })
-    }
-    start();
-    
+    });
+    this.setState({
+      valtotalinfo: arr1,
+      isloading: false
+    });
 
-
-
-
-
+    // async function asyncForEach(array, callback) {
+    //   for (let index = 0; index < array.length; index++) {
+    //     await callback(array[index], index, array);
+    //   }
+    // }
+    // const start = async () => {
+    //   let arr1 =[]
+    //   let count =0
+    //   await asyncForEach(this.state.validators, async (val) => {
+    //     console.log(val,count++)
+    //     let stakers = await api.derive.staking.info(val)
+    //     let stakeinfo = JSON.parse(stakers)
+    //     console.log(stakeinfo)
+    //     arr1.push({
+    //       valname:val,
+    //       valinfo:stakeinfo
+    //       })
+    //     console.log("arr1"+ arr1)
+    //     // stakeinfo.stakers.others.forEach(ele => {
+    //     //   if(ele.who === this.props.match.params.nominatorAddress)
+    //     //   {
+    //     //     arr1.push(val)
+    //     //     bonded += ele.value /Math.pow(10,15)
+    //     //   }
+    //     // })
+    //   });
+    //   console.log('Done');
+    //   console.log("arr1"+ arr1)
+    //   this.setState({
+    //     valtotalinfo:arr1,
+    //     isloading: false
+    //   })
+    // }
+    // start();
   }
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.ismounted = false;
   }
 
   render() {
-    console.log(this.props.history)
+    console.log(this.props.history);
     const arr = this.state.valtotalinfo;
     // const validatortext = "Validators: " + this.state.validators.length + "/" + this.state.totalvalidators
     // const arr1 = [1,2,3,4,5,6,7,8]
-    return (
-      this.state.isloading ? (<React.Fragment><div className="lds-ripple"><div></div><div></div></div><div className="lds-text">Waiting for API to be connected.....</div></React.Fragment>) : 
-      (
+    return this.state.isloading ? (
+      <React.Fragment>
+        <div className="lds-ripple">
+          <div></div>
+          <div></div>
+        </div>
+        <div className="lds-text">Waiting for API to be connected.....</div>
+      </React.Fragment>
+    ) : (
       <div className="container">
         {/* {console.log(this.state.validators.indexOf(this.state.lastAuthor))}  */}
 
@@ -118,7 +136,7 @@ class App extends React.Component {
           <Stage width={window.innerWidth} height={window.innerHeight}>
             {/* {console.log(window.innerWidth)} */}
             <Layer>
-            {/* <Parachains x={window.innerWidth} y={window.innerHeight} parachains={arr1}/> */}
+              {/* <Parachains x={window.innerWidth} y={window.innerHeight} parachains={arr1}/> */}
               {/*in  (90 - 1) "-1"  is to handle the deviation of hexagon wrt to validators */}
               {arr.map((person, index) => (
                 <Validator
@@ -131,12 +149,16 @@ class App extends React.Component {
                   x={
                     window.innerWidth +
                     360 *
-                      Math.cos((90 - 1 - (index * 360) / arr.length) * 0.0174533)
+                      Math.cos(
+                        (90 - 1 - (index * 360) / arr.length) * 0.0174533
+                      )
                   }
                   y={
                     window.innerHeight +
                     360 *
-                      Math.sin((90 - 1 - (index * 360) / arr.length) * 0.0174533)
+                      Math.sin(
+                        (90 - 1 - (index * 360) / arr.length) * 0.0174533
+                      )
                   }
                 />
               ))}
@@ -193,16 +215,16 @@ class App extends React.Component {
                 }
               />
               <Relay x={window.innerWidth} y={window.innerHeight} />
-              
             </Layer>
           </Stage>
         </div>
         <div className="bottombar">
-          <Bottombar start={this.state.start} activevalidators={this.state.validators.length}/>
+          <Bottombar
+            start={this.state.start}
+            activevalidators={this.state.validators.length}
+          />
         </div>
       </div>
-
-      )
     );
   }
 }
