4ea52a583ab8f7d0ae7fadd9114f70ce227c0554
Sahil <sahilnanda1995@gmail.com>
2019-10-30 19:58:23 +0530

    fixed issue#26

--- polkaviz-app/src/components/Validator.js
+++ polkaviz-app/src/components/Validator.js
@@ -2,69 +2,73 @@ import React from "react";
 import Rectangle from "./Rectangle";
 import Tail from "./Tail";
 
-
 class Validator extends React.Component {
-  
   render() {
-    let bondvalue = ""
-    let nomvalue = 0
-    let commission = ""
-    if(!this.props.isMainWrapper && this.props.intentions.length !== 0 ){
-      bondvalue = "Bonded: No Data found"
-      nomvalue = "Backed by: No Data found" 
-      if(!this.props.isKusama){
-      let totalvalue = this.props.valinfo.stakers.total / Math.pow(10,15)
-      let ownvalue = this.props.valinfo.stakers.own /Math.pow(10,15)
-      let totalbonded = 0
-      if(this.props.intentions.includes(this.props.validatorAddress)){
-        totalvalue = this.props.valinfo.stakingLedger.total
-        ownvalue = this.props.valinfo.stakingLedger.total
-      }
-      
-      totalbonded = totalvalue.toFixed(3)-ownvalue.toFixed(3)
-      bondvalue = "Bonded: " + ownvalue.toString().slice(0,5) + " (+ " + totalbonded.toString().slice(0,5) +" ) DOT"
-      nomvalue = "Backed by: " + this.props.valinfo.stakers.others.length + " nominators"
-    }
-      if(this.props.intentions.includes(this.props.validatorAddress)){
-      if(this.props.isKusama){
-        let value = this.props.valinfo.stakingLedger.active / Math.pow(10,12)
-        if(value > 1000)
-        {
-        value = value / Math.pow(10,3)
-        value=value.toFixed(3)
-        // value = value.split(".")[0]
-        value = value + "k"
+    let bondvalue = "";
+    let nomvalue = 0;
+    let commission = "";
+    if (!this.props.isMainWrapper && this.props.intentions.length !== 0) {
+      bondvalue = "Bonded: No Data found";
+      nomvalue = "Backed by: No Data found";
+      if (!this.props.isKusama) {
+        let totalvalue = this.props.valinfo.stakers.total / Math.pow(10, 15);
+        let ownvalue = this.props.valinfo.stakers.own / Math.pow(10, 15);
+        let totalbonded = 0;
+        if (this.props.intentions.includes(this.props.validatorAddress)) {
+          totalvalue = this.props.valinfo.stakingLedger.total;
+          ownvalue = this.props.valinfo.stakingLedger.total;
         }
-        else
-        value = value.toFixed(3)
-        // console.log(value)
-        bondvalue = "Bonded: " + value.toString() + " KSM"
 
-        let commissionvalue = this.props.valinfo.validatorPrefs.validatorPayment
-        commissionvalue = commissionvalue / Math.pow(10,12)
-        commissionvalue = commissionvalue.toFixed(3)
-        commission = "commission: " + commissionvalue + " KSM"
-              }
+        totalbonded = totalvalue.toFixed(3) - ownvalue.toFixed(3);
+        bondvalue =
+          "Bonded: " +
+          ownvalue.toString().slice(0, 5) +
+          " (+ " +
+          totalbonded.toString().slice(0, 5) +
+          " ) DOT";
+        nomvalue =
+          "Backed by: " +
+          this.props.valinfo.stakers.others.length +
+          " nominators";
+      }
+      if (this.props.intentions.includes(this.props.validatorAddress)) {
+        if (this.props.isKusama) {
+          let value =
+            this.props.valinfo.stakingLedger.active / Math.pow(10, 12);
+          if (value > 1000) {
+            value = value / Math.pow(10, 3);
+            value = value.toFixed(3);
+            // value = value.split(".")[0]
+            value = value + "k";
+          } else value = value.toFixed(3);
+          // console.log(value)
+          bondvalue = "Bonded: " + value.toString() + " KSM";
+
+          let commissionvalue = this.props.valinfo.validatorPrefs
+            .validatorPayment;
+          commissionvalue = commissionvalue / Math.pow(10, 12);
+          commissionvalue = commissionvalue.toFixed(3);
+          commission = "commission: " + commissionvalue + " KSM";
+        }
+      }
     }
-  }
-    let x1 = this.props.x
-    let y1 = this.props.y
-    let x2 = this.props.x
-    let y2 = this.props.y
-    let color ="#C31169"
-    let opacity =1
-    if(!this.props.isKusama){
-      color ="#9335A3"
+    let x1 = this.props.x;
+    let y1 = this.props.y;
+    let x2 = this.props.x;
+    let y2 = this.props.y;
+    let color = "#C31169";
+    let opacity = 1;
+    if (!this.props.isKusama) {
+      color = "#9335A3";
     }
-    if(!this.props.isMainWrapper){
-    if(this.props.intentions.includes(this.props.validatorAddress)){
-      
-      x1=((x1-window.innerWidth)/360*390)+window.innerWidth
-      y1=((y1-window.innerHeight)/360*390)+window.innerHeight
-      color="yellow"
-      opacity=0
+    if (!this.props.isMainWrapper) {
+      if (this.props.intentions.includes(this.props.validatorAddress)) {
+        x1 = ((x1 - window.innerWidth) / 360) * 390 + window.innerWidth;
+        y1 = ((y1 - window.innerHeight) / 360) * 390 + window.innerHeight;
+        color = "yellow";
+        opacity = 0;
+      }
     }
-  }
     return (
       <React.Fragment>
         <Tail
--- polkaviz-app/src/components/kusama/KusamaApp.js
+++ polkaviz-app/src/components/kusama/KusamaApp.js
@@ -4,9 +4,8 @@ import { Stage, Layer } from "react-konva";
 import Validator from "../Validator";
 import BlockAnimationNew from "./BlockAnimation-new";
 import Bottombar from "../Bottombar";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import { WsProvider, ApiPromise } from "@polkadot/api";
-
 
 class KusamaApp extends React.Component {
   constructor() {
@@ -27,43 +26,41 @@ class KusamaApp extends React.Component {
       kusamatotalValidators: 0,
       kusamafinalblock: 0,
       kusamaintentions: [],
-      kusamavalidatorandintentions:[],
+      kusamavalidatorandintentions: [],
       kusamatotalIssued: ""
     };
     this.ismounted = true;
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // window.location.reload()
-    this.createApi2()
+    this.createApi2();
   }
-  shouldComponentUpdate(nextProps,nextState){
-    if(this.state.kusamavalidators !== nextState.kusamavalidators || 
-      this.state.kusamavalidatorandintentions !==nextState.kusamavalidatorandintentions || 
-      this.state.kusamalastAuthor !== nextState.kusamalastAuthor || 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      this.state.kusamavalidators !== nextState.kusamavalidators ||
+      this.state.kusamavalidatorandintentions !==
+        nextState.kusamavalidatorandintentions ||
+      this.state.kusamalastAuthor !== nextState.kusamalastAuthor ||
       this.state.kusamafinalblock !== nextState.kusamafinalblock ||
       this.state.kusamabottombarinfo !== nextState.kusamabottombarinfo ||
       this.state.kusamaisloading !== nextState.kusamaisloading
-      )
-      return true
-    else
-      return false
+    )
+      return true;
+    else return false;
   }
-
-
 
   async createApi2() {
     let provider = new WsProvider("wss://kusama-rpc.polkadot.io");
     const apinew = await ApiPromise.create({ provider });
 
-
-    const balance = await apinew.query.balances.totalIssuance()
-    console.log(balance.toString())
-    const totalIssued = (balance.toString() / Math.pow(10, 18)).toFixed(3)
+    const balance = await apinew.query.balances.totalIssuance();
+    console.log(balance.toString());
+    const totalIssued = (balance.toString() / Math.pow(10, 18)).toFixed(3);
     if (this.ismounted) {
       this.setState({
-          kusamatotalIssued:totalIssued
-      })
+        kusamatotalIssued: totalIssued
+      });
     }
 
     await apinew.derive.chain.subscribeNewHeads(block => {
@@ -77,17 +74,17 @@ class KusamaApp extends React.Component {
       if (this.ismounted) {
         this.setState({
           kusamastart: start,
-          kusamafinalblock: blockNumber,
+          kusamafinalblock: blockNumber
         });
       }
     });
     let totalValidators = await apinew.query.staking.validatorCount();
-      // console.log("this", totalValidators.words["0"], totalValidators);
-      if (this.ismounted) {
-        this.setState({
-          kusamatotalValidators: totalValidators.words["0"]
-        });
-      }
+    // console.log("this", totalValidators.words["0"], totalValidators);
+    if (this.ismounted) {
+      this.setState({
+        kusamatotalValidators: totalValidators.words["0"]
+      });
+    }
 
     await apinew.query.session.validators(validators => {
       // console.log(validators)
@@ -103,19 +100,20 @@ class KusamaApp extends React.Component {
 
     const start = async () => {
       let arr1 = [];
-      let arr2 = [];
+      // let arr2 = [];
       // console.log(JSON.stringify(valinfo))
-      const intentions = await apinew.query.staking.validators()
-    console.log(JSON.parse(JSON.stringify(intentions)))
+      const intentions = await apinew.query.staking.validators();
+      const allvals = JSON.parse(JSON.stringify(intentions))[0];
+      console.log(JSON.parse(JSON.stringify(intentions)));
       const validatorstotalinfo = await Promise.all(
         this.state.kusamavalidators.map(val => apinew.derive.staking.info(val))
       );
 
-      console.log(JSON.parse(JSON.stringify(validatorstotalinfo)))
-      const intentionstotalinfo = await Promise.all(
-        JSON.parse(JSON.stringify(intentions))[0].map(intention => apinew.derive.staking.info(intention))
-      )
-      console.log(JSON.parse(JSON.stringify(intentionstotalinfo)))
+      console.log(JSON.parse(JSON.stringify(validatorstotalinfo)));
+      // const intentionstotalinfo = await Promise.all(
+      //   JSON.parse(JSON.stringify(intentions))[0].map(intention => apinew.derive.staking.info(intention))
+      // )
+      // console.log(JSON.parse(JSON.stringify(intentionstotalinfo)))
 
       arr1 = JSON.parse(JSON.stringify(validatorstotalinfo)).map(info => {
         // console.log(info);
@@ -124,31 +122,33 @@ class KusamaApp extends React.Component {
           valinfo: info
         };
       });
-console.log(arr1)
-      arr2 = JSON.parse(JSON.stringify(intentionstotalinfo)).map(info => {
-        // console.log(info);
+      console.log(arr1);
+      const arr2 = arr1.map(ele => ele.valname);
+      const arr3 = allvals.filter(e => !arr2.includes(e));
+      const intentionstotalinfo = await Promise.all(
+        arr3.map(val => apinew.derive.staking.info(val))
+      );
+      const arr4 = JSON.parse(JSON.stringify(intentionstotalinfo)).map(info => {
         return {
           valname: info.accountId,
           valinfo: info
         };
       });
-
-      let arr3 = [...arr1, ...arr2];
-
-      
+      console.log(arr4);
+      // let arr5 = this.state.validators.push(arr4);
+      let arr5 = [...arr1, ...arr4];
 
       if (this.ismounted) {
         // console.log("arr1",arr1)
         this.setState(
           {
             kusamavaltotalinfo: arr1,
-            kusamavalidatorandintentions:arr3,
-            kusamaintentions: JSON.parse(JSON.stringify(intentions))[0]
+            kusamavalidatorandintentions: arr5,
+            kusamaintentions: arr3
           }
           // () => this.getnominators2()
         );
       }
-  
     };
 
     start();
@@ -215,64 +215,68 @@ console.log(arr1)
   handlePolkavizClick = () => {
     document.body.style.cursor = "default";
     this.props.history.push({
-      pathname:"/",
-  })
-  }
-
-
+      pathname: "/"
+    });
+  };
 
   render() {
     // console.table(this.state)
     // console.log(this.state.kusamavalidators,"vals")
-    console.count("kusama rendered")
-    let arr = this.state.kusamavalidators
-    if(this.state.kusamavalidatorandintentions.length!==0){
+    console.count("kusama rendered");
+    let arr = this.state.kusamavalidators;
+    if (this.state.kusamavalidatorandintentions.length !== 0) {
       arr = this.state.kusamavalidatorandintentions;
     }
     // console.log(arr,"arr")
     // const intentionsarr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
     const intentionsarr = this.state.kusamaintentions;
     let bottombarobject2 = {
-        bottombarinfo: this.state.kusamabottombarinfo,
-        finalblock: this.state.kusamafinalblock,
-        validatorcount: this.state.kusamatotalValidators,
-        totalIssued:this.state.kusamatotalIssued.toString() + " M"
-      };
+      bottombarinfo: this.state.kusamabottombarinfo,
+      finalblock: this.state.kusamafinalblock,
+      validatorcount: this.state.kusamatotalValidators,
+      totalIssued: this.state.kusamatotalIssued.toString() + " M"
+    };
     // const validatortext = "Validators: " + this.state.validators.length + "/" + this.state.totalvalidators
     // const arr1 = [1,2,3,4,5,6,7,8]
-    return (
-      this.state.kusamavalidators.length === 0 ? (<React.Fragment><div className="lds-ripple"><div></div><div></div></div></React.Fragment>) : 
-      (
-
+    return this.state.kusamavalidators.length === 0 ? (
+      <React.Fragment>
+        <div className="lds-ripple">
+          <div></div>
+          <div></div>
+        </div>
+      </React.Fragment>
+    ) : (
       <div className="kusamacontainer">
-
         {/* <div className="heading">
           <h2>Kusama Network</h2>
         </div> */}
 
         <div className="nav-path">
-  <div className="nav-path-link" onClick={this.handlePolkavizClick}>Polkaviz</div>
-  <div>/</div>
-  <div className="nav-path-current">Kusama</div>
-  </div>
-
+          <div className="nav-path-link" onClick={this.handlePolkavizClick}>
+            Polkaviz
+          </div>
+          <div>/</div>
+          <div className="nav-path-current">Kusama</div>
+        </div>
 
         <div className="intentions">
           <div>Next Up:</div>
-          {intentionsarr.map((ele,index) => {
+          {intentionsarr.map((ele, index) => {
             return (
-            <div className="inten" key={index}>
-              <span className="valsign"></span>
-              {ele.toString().slice(0,8) + "......" + ele.toString().slice(-8)}
-            </div>
-            )
+              <div className="inten" key={index}>
+                <span className="valsign"></span>
+                {ele.toString().slice(0, 8) +
+                  "......" +
+                  ele.toString().slice(-8)}
+              </div>
+            );
           })}
         </div>
-        
+
         <div className="relay-circle">
           <Stage width={window.innerWidth} height={window.innerHeight}>
             <Layer>
-            {/* <Parachains x={window.innerWidth} y={window.innerHeight} parachains={arr1}/> */}
+              {/* <Parachains x={window.innerWidth} y={window.innerHeight} parachains={arr1}/> */}
               {/*in  (90 - 1) "-1"  is to handle the deviation of hexagon wrt to validators */}
               {arr.map((person, index) => (
                 <Validator
@@ -287,12 +291,16 @@ console.log(arr1)
                   x={
                     window.innerWidth +
                     360 *
-                      Math.cos((90 - 1 - (index * 360) / arr.length) * 0.0174533)
+                      Math.cos(
+                        (90 - 1 - (index * 360) / arr.length) * 0.0174533
+                      )
                   }
                   y={
                     window.innerHeight +
                     360 *
-                      Math.sin((90 - 1 - (index * 360) / arr.length) * 0.0174533)
+                      Math.sin(
+                        (90 - 1 - (index * 360) / arr.length) * 0.0174533
+                      )
                   }
                   isKusama={true}
                 />
@@ -300,10 +308,15 @@ console.log(arr1)
               {/* {console.log(this.state.bottombarobject.finalblock)}
               {console.log(this.state.previousBlock)} */}
               <BlockAnimationNew
-                key={this.state.kusamavalidators.indexOf(this.state.kusamalastAuthor)}
+                key={this.state.kusamavalidators.indexOf(
+                  this.state.kusamalastAuthor
+                )}
                 angle={
                   180 -
-                  (this.state.kusamavalidators.indexOf(this.state.kusamalastAuthor) * 360) /
+                  (this.state.kusamavalidators.indexOf(
+                    this.state.kusamalastAuthor
+                  ) *
+                    360) /
                     arr.length
                 }
                 x1={
@@ -311,7 +324,9 @@ console.log(arr1)
                   100 *
                     Math.cos(
                       (90 -
-                        (this.state.kusamavalidators.indexOf(this.state.kusamalastAuthor) *
+                        (this.state.kusamavalidators.indexOf(
+                          this.state.kusamalastAuthor
+                        ) *
                           360) /
                           arr.length) *
                         0.0174533
@@ -322,7 +337,9 @@ console.log(arr1)
                   100 *
                     Math.sin(
                       (90 -
-                        (this.state.kusamavalidators.indexOf(this.state.kusamalastAuthor) *
+                        (this.state.kusamavalidators.indexOf(
+                          this.state.kusamalastAuthor
+                        ) *
                           360) /
                           arr.length) *
                         0.0174533
@@ -333,7 +350,9 @@ console.log(arr1)
                   160 *
                     Math.cos(
                       (90 -
-                        (this.state.kusamavalidators.indexOf(this.state.kusamalastAuthor) *
+                        (this.state.kusamavalidators.indexOf(
+                          this.state.kusamalastAuthor
+                        ) *
                           360) /
                           arr.length) *
                         0.0174533
@@ -344,24 +363,33 @@ console.log(arr1)
                   160 *
                     Math.sin(
                       (90 -
-                        (this.state.kusamavalidators.indexOf(this.state.kusamalastAuthor) *
+                        (this.state.kusamavalidators.indexOf(
+                          this.state.kusamalastAuthor
+                        ) *
                           360) /
                           arr.length) *
                         0.0174533
                     )
                 }
               />
-              <Relay x={window.innerWidth} y={window.innerHeight} isKusama={true}/>
-              
+              <Relay
+                x={window.innerWidth}
+                y={window.innerHeight}
+                isKusama={true}
+              />
             </Layer>
           </Stage>
         </div>
         <div className="bottombar">
-          <Bottombar start={this.state.kusamastart} activevalidators={this.state.kusamavalidators.length} validatorcount={this.state.kusamavalidatorcount} bottombarobject ={bottombarobject2} isKusama={true}/>
+          <Bottombar
+            start={this.state.kusamastart}
+            activevalidators={this.state.kusamavalidators.length}
+            validatorcount={this.state.kusamavalidatorcount}
+            bottombarobject={bottombarobject2}
+            isKusama={true}
+          />
         </div>
       </div>
-
-      )
     );
   }
 }
