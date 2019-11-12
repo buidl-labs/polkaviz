13e063ff400428f50ce516569d90b22aabca085a
saumyakaran <saumya942@gmail.com>
2019-11-12 21:30:21 +0530

    minor ui changes

--- polkadot-reward-calculator-app/src/components/reward_calculator_components/CalculatorForm.jsx
+++ polkadot-reward-calculator-app/src/components/reward_calculator_components/CalculatorForm.jsx
@@ -1,99 +1,84 @@
 import React from "react";
 import {
-    Box,
-    Stack,
-    Input,
-    FormControl,
-    Heading,
-    FormLabel
+	Stack,
+	Input,
+	FormControl,
+	FormLabel,
+	Box,
+	Stat,
+	StatLabel,
+	StatNumber,
+	StatHelpText
 } from "@chakra-ui/core";
 
 class CalculatorForm extends React.Component {
-    render() {
-        const {
+	render() {
+		const {
 			colorMode,
-            stakeAmount,
-            dailyEarning,
-            maxReward,
-            validatorAddress,
-            validatorPayment,
-            handleStateChange
-        } = this.props;
-        return (
-            <React.Fragment>
-                <Box>
-                    With a <b>{stakeAmount} DOT</b> stake you could be earning upto: &nbsp;
-                    <Box
-                        backgroundColor="tomato"
-                        width="fit-content"
-                        borderRadius={4}
-                        px={4}
-                        py={2}
-                        mt={3}
-                    >
-                        <Heading
-                            as="h3"
-                            size="lg"
-                            color={colorMode === "light" ? "#fff" : "gray.900"}
-                        >
-                            {maxReward === 0 ? "0" : maxReward} DOTs
-                            daily!
-                        </Heading>
-                    </Box>
-                </Box>
-                <Stack mt={8} spacing={6} direction="horizontal">
-                    <FormControl mr={8}>
-                        <FormLabel htmlFor="stakeAmount">
-                            Stake Amount (in DOTs)
-                        </FormLabel>
-                        <Input
-                            name="stakeAmount"
-                            placeholder="No. of DOTs"
-                            value={stakeAmount}
-                            onChange={handleStateChange}
-                        />
-                    </FormControl>
+			stakeAmount,
+			dailyEarning,
+			maxReward,
+			validatorAddress,
+			validatorPayment,
+			handleStateChange
+		} = this.props;
+		return (
+			<Box>
+				<Stat mt={4} p={4} textAlign="center" backgroundColor="tomato" borderRadius={4}>
+					<StatLabel>Earn upto</StatLabel>
+					<StatNumber>{maxReward === 0 ? "0" : maxReward} DOTs</StatNumber>
+					<StatHelpText>for {stakeAmount} DOT(s) stake</StatHelpText>
+				</Stat>
+				<Stack mt={8} spacing={6} direction="horizontal">
+					<FormControl flexGrow={1}>
+						<FormLabel htmlFor="stakeAmount">Stake Amount (in DOTs)</FormLabel>
+						<Input
+							name="stakeAmount"
+							placeholder="No. of DOTs"
+							value={stakeAmount}
+							onChange={handleStateChange}
+						/>
+					</FormControl>
+				</Stack>
+				<Stack spacing={6} direction="horizontal">
+					<FormControl mr={8}>
+						<FormLabel htmlFor="validatorAddress">
+							Validator's Address
+						</FormLabel>
+						<Input
+							name="validatorAddress"
+							placeholder="Validator's Address"
+							value={validatorAddress}
+							onChange={handleStateChange}
+						/>
+					</FormControl>
 
-                    <FormControl>
-                        <FormLabel htmlFor="dailyEarning">
-                            Daily Earning:
-                        </FormLabel>
-                        <Input
-                            name="dailyEarning"
-                            placeholder="Daily Earning"
-                            value={dailyEarning}
-                            readOnly
-                        />
-                    </FormControl>
-                </Stack>
-                <Stack spacing={6} direction="horizontal">
-                    <FormControl mr={8}>
-                        <FormLabel htmlFor="validatorAddress">
-                            Validator's Address
-                        </FormLabel>
-                        <Input
-                            name="validatorAddress"
-                            placeholder="Validator's Address"
-                            value={validatorAddress}
-                            onChange={handleStateChange}
-                        />
-                    </FormControl>
-
-                    <FormControl>
-                        <FormLabel htmlFor="validatorPayment">
-                            Validator payment/commission (in DOT’s)
-                        </FormLabel>
-                        <Input
-                            name="validatorPayment"
-                            placeholder="Commision in DOTs (optional)"
-                            value={validatorPayment}
-                            onChange={handleStateChange}
-                        />
-                    </FormControl>
-                </Stack>
-            </React.Fragment>
-        );
-    }
+					<FormControl>
+						<FormLabel htmlFor="validatorPayment">
+							Validator payment/commission (in DOT’s)
+						</FormLabel>
+						<Input
+							name="validatorPayment"
+							placeholder="Commision in DOTs (optional)"
+							value={validatorPayment}
+							onChange={handleStateChange}
+						/>
+					</FormControl>
+				</Stack>
+				<Stack spacing={6}>
+					<FormControl>
+						<FormLabel htmlFor="dailyEarning">Daily Earning:</FormLabel>
+						<Input
+							name="dailyEarning"
+							placeholder="Daily Earning"
+							value={dailyEarning}
+							readOnly
+						/>
+					</FormControl>
+				</Stack>
+			</Box>
+		);
+	}
 }
 
 export default CalculatorForm;
--- polkadot-reward-calculator-app/src/components/reward_calculator_components/RewardCalculatorApp.jsx
+++ polkadot-reward-calculator-app/src/components/reward_calculator_components/RewardCalculatorApp.jsx
@@ -1,75 +1,69 @@
 import React from "react";
-import {
-    Stack,
-    Heading,
-    Text,
-    Box,
-    Link
-} from "@chakra-ui/core";
+import { Stack, Heading, Text, Box, Link } from "@chakra-ui/core";
 import RewardChart from "./RewardChart";
 import CalculatorForm from "./CalculatorForm";
 
 class RewardCalculatorApp extends React.Component {
-    render() {
-        const {
-            validatorData,
-            stakeAmount,
-            dailyEarning,
-            maxReward,
-            validatorAddress,
-            validatorPayment,
-            colorMode,
-            handleStateChange,
-            handleChartClick,
-            handleMaxReward
-        } = this.props;
-        return (
-					<Stack p={8} pt={2}>
-						<Box>
-							<Heading>Alexander Network</Heading>
-							<Text fontSize="xl">
-								Rewards calculator for Alexander Network inside{" "}
-								<Link
-									href="https://polkadot.network/"
-									color="teal.500"
-									isExternal
-								>
-									Polkadot Ecosystem
-								</Link>{" "}
-							</Text>
-						</Box>
-						<Box>
-							<Box
-								my={8}
-								p={8}
-								bg={colorMode === "light" ? "gray.200" : "gray.900"}
-								h="fit-content"
-							>
-								<CalculatorForm
-									colorMode={colorMode}
-									stakeAmount={stakeAmount}
-									dailyEarning={dailyEarning}
-									maxReward={maxReward}
-									validatorAddress={validatorAddress}
-									validatorPayment={validatorPayment}
-									handleStateChange={handleStateChange}
-								/>
-							</Box>
-							<Text>
-								The graph displays expected daily rewards for nominators if
-								equal value is staked to each validator in current system (in
-								DOTs). Actual rewards may vary.
-							</Text>
-							<RewardChart
-								colorMode={colorMode}
-								validatorData={validatorData}
-								handleChartClick={handleChartClick}
-								handleMaxReward={handleMaxReward}
-							/>
-						</Box>
-					</Stack>
-				);
-    }
+	render() {
+		const {
+			validatorData,
+			stakeAmount,
+			dailyEarning,
+			maxReward,
+			validatorAddress,
+			validatorPayment,
+			colorMode,
+			handleStateChange,
+			handleChartClick,
+			handleMaxReward
+		} = this.props;
+		return (
+			<Stack p={8} pt={2}>
+				<Box>
+					<Heading>Alexander Network</Heading>
+					<Text fontSize="xl">
+						Rewards calculator for Alexander Network inside{" "}
+						<Link href="https://polkadot.network/" color="teal.500" isExternal>
+							Polkadot Ecosystem
+						</Link>{" "}
+					</Text>
+				</Box>
+				<Box>
+					<Box
+						my={8}
+						p={8}
+						pt={4}
+						bg={colorMode === "light" ? "gray.200" : "gray.900"}
+						h="fit-content"
+						display="flex"
+						flexDirection="column"
+						alignItems="center"
+					>
+						<CalculatorForm
+							colorMode={colorMode}
+							stakeAmount={stakeAmount}
+							dailyEarning={dailyEarning}
+							maxReward={maxReward}
+							validatorAddress={validatorAddress}
+							validatorPayment={validatorPayment}
+							handleStateChange={handleStateChange}
+						/>
+					</Box>
+					<Text>
+						The graph displays expected daily rewards for nominators if equal
+						value is staked to each validator in current system (in DOTs).
+						Actual rewards may vary.
+					</Text>
+					<RewardChart
+						colorMode={colorMode}
+						validatorData={validatorData}
+						handleChartClick={handleChartClick}
+						handleMaxReward={handleMaxReward}
+					/>
+				</Box>
+			</Stack>
+		);
+	}
 }
 
 export default RewardCalculatorApp;
