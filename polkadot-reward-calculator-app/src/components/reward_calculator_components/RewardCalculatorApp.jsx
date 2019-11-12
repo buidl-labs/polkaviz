6ea68dbf5ee6f93b36ececb8e75b1dba0f1f73b4
saumyakaran <saumya942@gmail.com>
2019-11-12 20:35:15 +0530

    chart ui fixes

--- polkadot-reward-calculator-app/src/components/reward_calculator_components/RewardCalculatorApp.jsx
+++ polkadot-reward-calculator-app/src/components/reward_calculator_components/RewardCalculatorApp.jsx
@@ -4,8 +4,7 @@ import {
     Heading,
     Text,
     Box,
-    Alert,
-    AlertIcon,
+    Link
 } from "@chakra-ui/core";
 import RewardChart from "./RewardChart";
 import CalculatorForm from "./CalculatorForm";
@@ -25,43 +24,51 @@ class RewardCalculatorApp extends React.Component {
             handleMaxReward
         } = this.props;
         return (
-            <Stack p={8} pt={2}>
-                <Box>
-                    <Heading>Polkadot Network</Heading>
-                    <Text fontSize="xl">Rewards distribution</Text>
-                </Box>
-                <Box>
-                    <Box
-                        my={8}
-                        p={8}
-                        bg={colorMode === "light" ? "gray.200" : "gray.900"}
-                        h="fit-content"
-                    >
-                        <CalculatorForm
-                            colorMode={colorMode}
-                            stakeAmount={stakeAmount}
-                            dailyEarning={dailyEarning}
-                            maxReward={maxReward}
-                            validatorAddress={validatorAddress}
-                            validatorPayment={validatorPayment}
-                            handleStateChange={handleStateChange}
-                        />
-                    </Box>
-                    <Alert status="warning">
-                        <AlertIcon />
-                        The graph displays expected daily rewards for
-                        nominators if equal value is staked to each validator in
-                        current system (in DOTs). Actual rewards may vary.
-                    </Alert>
-                    <RewardChart
-                        colorMode={colorMode}
-                        validatorData={validatorData}
-                        handleChartClick={handleChartClick}
-                        handleMaxReward={handleMaxReward}
-                    />
-                </Box>
-            </Stack>
-        );
+					<Stack p={8} pt={2}>
+						<Box>
+							<Heading>Alexander Network</Heading>
+							<Text fontSize="xl">
+								Rewards calculator for Alexander Network inside{" "}
+								<Link
+									href="https://polkadot.network/"
+									color="teal.500"
+									isExternal
+								>
+									Polkadot Ecosystem
+								</Link>{" "}
+							</Text>
+						</Box>
+						<Box>
+							<Box
+								my={8}
+								p={8}
+								bg={colorMode === "light" ? "gray.200" : "gray.900"}
+								h="fit-content"
+							>
+								<CalculatorForm
+									colorMode={colorMode}
+									stakeAmount={stakeAmount}
+									dailyEarning={dailyEarning}
+									maxReward={maxReward}
+									validatorAddress={validatorAddress}
+									validatorPayment={validatorPayment}
+									handleStateChange={handleStateChange}
+								/>
+							</Box>
+							<Text>
+								The graph displays expected daily rewards for nominators if
+								equal value is staked to each validator in current system (in
+								DOTs). Actual rewards may vary.
+							</Text>
+							<RewardChart
+								colorMode={colorMode}
+								validatorData={validatorData}
+								handleChartClick={handleChartClick}
+								handleMaxReward={handleMaxReward}
+							/>
+						</Box>
+					</Stack>
+				);
     }
 }
 
