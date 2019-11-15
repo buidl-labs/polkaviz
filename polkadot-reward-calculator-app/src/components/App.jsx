b1e068b73787d05aff19ca2ffeebf3cdad184d0b
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2019-11-15 13:39:52 +0530

    loading progress added

--- polkadot-reward-calculator-app/src/components/App.jsx
+++ polkadot-reward-calculator-app/src/components/App.jsx
@@ -5,246 +5,255 @@ import RewardCalculatorApp from "./reward_calculator_components/RewardCalculator
 import FAQs from "./FAQs";
 
 function App() {
-	const { colorMode, toggleColorMode } = useColorMode();
-	const [state, setState] = React.useState({
-		validators: [],
-		stakeAmount: 1,
-		chartStake: 1,
-		maxReward: 0,
-		validatorData: [],
-		validatorAddress: "",
-		validatorPayment: "",
-		validatorPoolReward: "",
-		dailyEarning: "",
-		mounted: false
-	});
+    const { colorMode, toggleColorMode } = useColorMode();
+    const [state, setState] = React.useState({
+        validators: [],
+        stakeAmount: 1,
+        chartStake: 1,
+        maxReward: "",
+        validatorData: [],
+        validatorAddress: "",
+        validatorPayment: "",
+        validatorPoolReward: "",
+        dailyEarning: "",
+        mounted: false,
+        apiConnected: false
+    });
 
-	const createApi = React.useCallback(async () => {
-		console.clear();
-		const provider = new WsProvider("wss://poc3-rpc.polkadot.io");
-		const api = await ApiPromise.create({ provider: provider });
+    const createApi = React.useCallback(async () => {
+        console.clear();
+        const provider = new WsProvider("wss://poc3-rpc.polkadot.io");
+        const api = await ApiPromise.create({ provider: provider });
 
-		const [currentSessionReward, validators] = await Promise.all([
-			api.query.staking.currentSessionReward(),
-			api.query.session.validators()
-		]);
-		const validatorPoolReward = (currentSessionReward * 6) / 10 ** 12;
-		const validatorAddressList = validators.map(async validator => {
-			const currentStakeInfo = await api.derive.staking.info(validator);
-			const currentTotalStake =
-				currentStakeInfo.stakers.total.toString() / 10 ** 15;
-			const currentValidatorPayment =
-				(await currentStakeInfo.validatorPrefs.validatorPayment.toString()) /
-				10 ** 15;
-			return {
-				address: validator,
-				payment: currentValidatorPayment,
-				totalStake: currentTotalStake,
-				stakeInfo: currentStakeInfo,
-				accountId: currentStakeInfo.accountId.toString(),
-				stashId: currentStakeInfo.stashId.toString()
-			};
-		});
-		const validatorList = await Promise.all(validatorAddressList);
-		if (state.mounted) {
-			setState(state => ({
-				...state,
-				validators: validatorList,
-				validatorPoolReward: validatorPoolReward
-			}));
-		}
-	}, [state.mounted]);
+        const [currentSessionReward, validators] = await Promise.all([
+            api.query.staking.currentSessionReward(),
+            api.query.session.validators()
+        ]);
+        const validatorPoolReward = (currentSessionReward * 6) / 10 ** 12;
+        const validatorAddressList = validators.map(async validator => {
+            const currentStakeInfo = await api.derive.staking.info(validator);
+            const currentTotalStake =
+                currentStakeInfo.stakers.total.toString() / 10 ** 15;
+            const currentValidatorPayment =
+                (await currentStakeInfo.validatorPrefs.validatorPayment.toString()) /
+                10 ** 15;
+            return {
+                address: validator,
+                payment: currentValidatorPayment,
+                totalStake: currentTotalStake,
+                stakeInfo: currentStakeInfo,
+                accountId: currentStakeInfo.accountId.toString(),
+                stashId: currentStakeInfo.stashId.toString()
+            };
+        });
+        const validatorList = await Promise.all(validatorAddressList);
+        if (state.mounted) {
+            setState(state => ({
+                ...state,
+                validators: validatorList,
+                validatorPoolReward: validatorPoolReward,
+                apiConnected: true
+            }));
+        }
+    }, [state.mounted]);
 
-	const calculateReward = React.useCallback(() => {
-		const EXPECTED_NUM_OF_ERA_PER_DAY = 24;
-		const selectedValidator = state.validators.find(validator => {
-			return (
-				validator.address.toString() === state.validatorAddress.toString() ||
-				validator.stashId.toString() === state.validatorAddress.toString()
-			);
-		});
-		if (selectedValidator !== undefined) {
-			const currentUserStakeFraction =
-				parseFloat(state.stakeAmount) /
-				(parseFloat(state.stakeAmount) + selectedValidator.totalStake);
-			const currentExpectedReward =
-				(state.validatorPoolReward -
-					(state.validatorPayment === ""
-						? selectedValidator.payment
-						: state.validatorPayment)) *
-				currentUserStakeFraction;
+    const calculateReward = React.useCallback(() => {
+        const EXPECTED_NUM_OF_ERA_PER_DAY = 24;
+        const selectedValidator = state.validators.find(validator => {
+            return (
+                validator.address.toString() ===
+                    state.validatorAddress.toString() ||
+                validator.stashId.toString() ===
+                    state.validatorAddress.toString()
+            );
+        });
+        if (selectedValidator !== undefined) {
+            const currentUserStakeFraction =
+                parseFloat(state.stakeAmount) /
+                (parseFloat(state.stakeAmount) + selectedValidator.totalStake);
+            const currentExpectedReward =
+                (state.validatorPoolReward -
+                    (state.validatorPayment === ""
+                        ? selectedValidator.payment
+                        : state.validatorPayment)) *
+                currentUserStakeFraction;
 
-			const currentDailyEarning =
-				currentExpectedReward <= 0 || isNaN(currentExpectedReward)
-					? 0
-					: currentExpectedReward * EXPECTED_NUM_OF_ERA_PER_DAY;
-			if (state.mounted) {
-				setState(state => ({
-					...state,
-					selectedValidator: selectedValidator,
-					dailyEarning: currentDailyEarning,
-					validatorPayment: selectedValidator.payment
-				}));
-			}
-		}
-	}, [
-		state.stakeAmount,
-		state.validatorPoolReward,
-		state.mounted,
-		state.validatorAddress,
-		state.validators,
-		state.validatorPayment
-	]);
+            const currentDailyEarning =
+                currentExpectedReward <= 0 || isNaN(currentExpectedReward)
+                    ? 0
+                    : currentExpectedReward * EXPECTED_NUM_OF_ERA_PER_DAY;
+            if (state.mounted) {
+                setState(state => ({
+                    ...state,
+                    selectedValidator: selectedValidator,
+                    dailyEarning: currentDailyEarning,
+                    validatorPayment: selectedValidator.payment
+                }));
+            }
+        }
+    }, [
+        state.stakeAmount,
+        state.validatorPoolReward,
+        state.mounted,
+        state.validatorAddress,
+        state.validators,
+        state.validatorPayment
+    ]);
 
-	const validatorData = React.useCallback(() => {
-		const EXPECTED_NUM_OF_ERA_PER_DAY = 24;
-		const validatorData = state.validators.map(validator => {
-			const currentUserStakeFraction =
-				parseFloat(state.stakeAmount) /
-				(parseFloat(state.stakeAmount) + validator.totalStake);
-			const currentExpectedReward =
-				(state.validatorPoolReward - validator.payment) *
-				currentUserStakeFraction;
-			const currentDailyEarning =
-				currentExpectedReward <= 0 || isNaN(currentExpectedReward)
-					? 0
-					: currentExpectedReward * EXPECTED_NUM_OF_ERA_PER_DAY;
-			return {
-				address:
-					validator.stashId.toString().slice(0, 4) +
-					"...." +
-					validator.stashId.toString().slice(-5),
-				reward: currentDailyEarning,
-				completeAddress: validator.address.toString(),
-				stashId: validator.stashId.toString()
-			};
-		});
-		if (state.mounted) {
-			setState(state => ({
-				...state,
-				validatorData: validatorData
-			}));
-		}
-	}, [
-		state.stakeAmount,
-		state.validatorPoolReward,
-		state.validators,
-		state.mounted
-	]);
+    const validatorData = React.useCallback(() => {
+        const EXPECTED_NUM_OF_ERA_PER_DAY = 24;
+        const validatorData = state.validators.map(validator => {
+            const currentUserStakeFraction =
+                parseFloat(state.stakeAmount) /
+                (parseFloat(state.stakeAmount) + validator.totalStake);
+            const currentExpectedReward =
+                (state.validatorPoolReward - validator.payment) *
+                currentUserStakeFraction;
+            const currentDailyEarning =
+                currentExpectedReward <= 0 || isNaN(currentExpectedReward)
+                    ? 0
+                    : currentExpectedReward * EXPECTED_NUM_OF_ERA_PER_DAY;
+            return {
+                address:
+                    validator.stashId.toString().slice(0, 4) +
+                    "...." +
+                    validator.stashId.toString().slice(-5),
+                reward: currentDailyEarning,
+                completeAddress: validator.address.toString(),
+                stashId: validator.stashId.toString()
+            };
+        });
+        if (state.mounted) {
+            setState(state => ({
+                ...state,
+                validatorData: validatorData
+            }));
+        }
+    }, [
+        state.stakeAmount,
+        state.validatorPoolReward,
+        state.validators,
+        state.mounted
+    ]);
 
-	const calculateMaxReward = (acc, curr) => {
-		if (acc > curr.reward) {
-		} else {
-			acc = curr.reward;
-		}
-		return acc;
-	};
+    const calculateMaxReward = (acc, curr) => {
+        if (acc > curr.reward) {
+        } else {
+            acc = curr.reward;
+        }
+        return acc;
+    };
 
-	React.useEffect(() => {
-		setState(state => ({
-			...state,
-			mounted: true
-		}));
-		createApi();
-		calculateReward();
-		validatorData();
-	}, [createApi, calculateReward, validatorData]);
+    React.useEffect(() => {
+        setState(state => ({
+            ...state,
+            mounted: true
+        }));
+        createApi();
+        calculateReward();
+        validatorData();
+    }, [createApi, calculateReward, validatorData]);
 
-	React.useEffect(() => {
-		const maxReward = state.validatorData.reduce(calculateMaxReward, 0);
-		if (state.mounted) {
-			setState(state => ({
-				...state,
-				maxReward: maxReward
-			}));
-		}
-	}, [
-		state.maxReward,
-		state.mounted,
-		state.validators,
-		state.validatorData,
-		state.validatorPoolReward
-	]);
+    React.useEffect(() => {
+        const maxReward = state.validatorData.reduce(calculateMaxReward, 0);
+        if (state.mounted && state.apiConnected) {
+            setState(state => ({
+                ...state,
+                maxReward: maxReward
+            }));
+        }
+    }, [
+        state.apiConnected,
+        state.maxReward,
+        state.mounted,
+        state.validators,
+        state.validatorData,
+        state.validatorPoolReward
+    ]);
 
-	const handleStateChange = e => {
-		const { name, value } = e.target;
-		setState(state => ({
-			...state,
-			[name]: value
-		}));
-	};
+    const handleStateChange = e => {
+        const { name, value } = e.target;
+        setState(state => ({
+            ...state,
+            [name]: value
+        }));
+    };
 
-	const handleStakeChange = chartStake => {
-		setState(state => ({
-			...state,
-			chartStake: chartStake
-		}));
-	};
+    const handleStakeChange = chartStake => {
+        setState(state => ({
+            ...state,
+            chartStake: chartStake
+        }));
+    };
 
-	const handleChartClick = address => {
-		setState(state => ({
-			...state,
-			validatorAddress: address
-		}));
-	};
+    const handleChartClick = address => {
+        setState(state => ({
+            ...state,
+            validatorAddress: address
+        }));
+    };
 
-	const handleMaxReward = reward => {
-		setState(state => ({
-			...state,
-			maxReward: reward
-		}));
-	};
+    const handleMaxReward = reward => {
+        setState(state => ({
+            ...state,
+            maxReward: reward
+        }));
+    };
 
-	return (
-		<Flex
-			className="App"
-			maxW="960px"
-			justify="center"
-			direction="column"
-			m="auto"
-			pb={8}
-		>
-			<Flex justify="flex-end" p={4} pb={2}>
-				<a href="mailto:sahil@thevantageproject.com">
-					<IconButton
-						icon="email"
-						size="lg"
-						mr={2}
-						backgroundColor={colorMode === "light" ? "#fff" : "gray.800"}
-					/>
-				</a>
-				<IconButton
-					aria-label={
-						colorMode === "light"
-							? "Switch to dark mode"
-							: "Switch to light mode"
-					}
-					icon={colorMode === "light" ? "moon" : "sun"}
-					size="lg"
-					onClick={toggleColorMode}
-					backgroundColor={colorMode === "light" ? "#fff" : "gray.800"}
-				/>
-			</Flex>
-			<RewardCalculatorApp
-				colorMode={colorMode}
-				validatorAddress={state.validatorAddress}
-				validatorData={state.validatorData}
-				validatorPayment={state.validatorPayment}
-				stakeAmount={state.stakeAmount}
-				maxReward={state.maxReward}
-				chartStake={state.chartStake}
-				dailyEarning={state.dailyEarning}
-				handleStateChange={handleStateChange}
-				handleStakeChange={handleStakeChange}
-				handleChartClick={handleChartClick}
-				handleMaxReward={handleMaxReward}
-			/>
-			<Heading as="h3" size="xl" textAlign="center" mb={3}>
-				FAQs
-			</Heading>
-			<FAQs />
-		</Flex>
-	);
+    return (
+        <Flex
+            className="App"
+            maxW="960px"
+            justify="center"
+            direction="column"
+            m="auto"
+            pb={8}
+        >
+            <Flex justify="flex-end" p={4} pb={2}>
+                <a href="mailto:sahil@thevantageproject.com">
+                    <IconButton
+                        icon="email"
+                        size="lg"
+                        mr={2}
+                        backgroundColor={
+                            colorMode === "light" ? "#fff" : "gray.800"
+                        }
+                    />
+                </a>
+                <IconButton
+                    aria-label={
+                        colorMode === "light"
+                            ? "Switch to dark mode"
+                            : "Switch to light mode"
+                    }
+                    icon={colorMode === "light" ? "moon" : "sun"}
+                    size="lg"
+                    onClick={toggleColorMode}
+                    backgroundColor={
+                        colorMode === "light" ? "#fff" : "gray.800"
+                    }
+                />
+            </Flex>
+            <RewardCalculatorApp
+                colorMode={colorMode}
+                validatorAddress={state.validatorAddress}
+                validatorData={state.validatorData}
+                validatorPayment={state.validatorPayment}
+                stakeAmount={state.stakeAmount}
+                maxReward={state.maxReward}
+                chartStake={state.chartStake}
+                dailyEarning={state.dailyEarning}
+                handleStateChange={handleStateChange}
+                handleStakeChange={handleStakeChange}
+                handleChartClick={handleChartClick}
+                handleMaxReward={handleMaxReward}
+            />
+            <Heading as="h3" size="xl" textAlign="center" mb={3}>
+                FAQs
+            </Heading>
+            <FAQs />
+        </Flex>
+    );
 }
 
 export default App;
--- polkadot-reward-calculator-app/src/components/reward_calculator_components/CalculatorForm.jsx
+++ polkadot-reward-calculator-app/src/components/reward_calculator_components/CalculatorForm.jsx
@@ -1,84 +1,105 @@
 import React from "react";
 import {
-	Stack,
-	Input,
-	FormControl,
-	FormLabel,
-	Box,
-	Stat,
-	StatLabel,
-	StatNumber,
-	StatHelpText
+    Stack,
+    Input,
+    FormControl,
+    FormLabel,
+    Box,
+    Stat,
+    StatLabel,
+    StatNumber,
+    StatHelpText,
+    CircularProgress
 } from "@chakra-ui/core";
 
 class CalculatorForm extends React.Component {
-	render() {
-		const {
-			colorMode,
-			stakeAmount,
-			dailyEarning,
-			maxReward,
-			validatorAddress,
-			validatorPayment,
-			handleStateChange
-		} = this.props;
-		return (
-			<Box>
-				<Stat mt={4} p={4} textAlign="center" backgroundColor="tomato" borderRadius={4}>
-					<StatLabel>Earn upto</StatLabel>
-					<StatNumber>{maxReward === 0 ? "0" : maxReward} DOTs</StatNumber>
-					<StatHelpText>for {stakeAmount} DOT(s) stake</StatHelpText>
-				</Stat>
-				<Stack mt={8} spacing={6} direction="horizontal">
-					<FormControl flexGrow={1}>
-						<FormLabel htmlFor="stakeAmount">Stake Amount (in DOTs)</FormLabel>
-						<Input
-							name="stakeAmount"
-							placeholder="No. of DOTs"
-							value={stakeAmount}
-							onChange={handleStateChange}
-						/>
-					</FormControl>
-				</Stack>
-				<Stack spacing={6} direction="horizontal">
-					<FormControl mr={8}>
-						<FormLabel htmlFor="validatorAddress">
-							Validator's Address
-						</FormLabel>
-						<Input
-							name="validatorAddress"
-							placeholder="Validator's Address"
-							value={validatorAddress}
-							onChange={handleStateChange}
-						/>
-					</FormControl>
+    render() {
+        const {
+            stakeAmount,
+            dailyEarning,
+            maxReward,
+            validatorAddress,
+            validatorPayment,
+            handleStateChange
+        } = this.props;
+        return (
+            <Box>
+                <Stat
+                    mt={4}
+                    p={4}
+                    textAlign="center"
+                    backgroundColor="tomato"
+                    borderRadius={4}
+                >
+                    <StatLabel>Earn upto</StatLabel>
+                    <StatNumber>
+                        {maxReward === "" ? (
+                            <CircularProgress
+                                isIndeterminate
+                                as="span"
+                                color="white"
+                                my={3}
+                            ></CircularProgress>
+                        ) : (
+                            `${maxReward} DOTs`
+                        )}
+                    </StatNumber>
+                    <StatHelpText>for {stakeAmount} DOT(s) stake</StatHelpText>
+                </Stat>
+                <Stack mt={8} spacing={6} direction="horizontal">
+                    <FormControl flexGrow={1}>
+                        <FormLabel htmlFor="stakeAmount">
+                            Stake Amount (in DOTs)
+                        </FormLabel>
+                        <Input
+                            name="stakeAmount"
+                            placeholder="No. of DOTs"
+                            value={stakeAmount}
+                            onChange={handleStateChange}
+                        />
+                    </FormControl>
+                </Stack>
+                <Stack spacing={6} direction="horizontal">
+                    <FormControl mr={8}>
+                        <FormLabel htmlFor="validatorAddress">
+                            Validator's Address
+                        </FormLabel>
+                        <Input
+                            name="validatorAddress"
+                            placeholder="Validator's Address"
+                            value={validatorAddress}
+                            onChange={handleStateChange}
+                        />
+                    </FormControl>
 
-					<FormControl>
-						<FormLabel htmlFor="validatorPayment">
-							Validator payment/commission (in DOT’s)
-						</FormLabel>
-						<Input
-							name="validatorPayment"
-							placeholder="Commision in DOTs (optional)"
-							value={validatorPayment}
-							onChange={handleStateChange}
-						/>
-					</FormControl>
-				</Stack>
-				<Stack spacing={6}>
-					<FormControl>
-						<FormLabel htmlFor="dailyEarning">Daily Earning:</FormLabel>
-						<Input
-							name="dailyEarning"
-							placeholder="Daily Earning"
-							value={dailyEarning}
-							readOnly
-						/>
-					</FormControl>
-				</Stack>
-			</Box>
-		);
-	}
+                    <FormControl>
+                        <FormLabel htmlFor="validatorPayment">
+                            Validator payment/commission (in DOT’s)
+                        </FormLabel>
+                        <Input
+                            name="validatorPayment"
+                            placeholder="Commision in DOTs (optional)"
+                            value={validatorPayment}
+                            onChange={handleStateChange}
+                        />
+                    </FormControl>
+                </Stack>
+                <Stack spacing={6}>
+                    <FormControl>
+                        <FormLabel htmlFor="dailyEarning">
+                            Daily Earning:
+                        </FormLabel>
+                        <Input
+                            name="dailyEarning"
+                            placeholder="Daily Earning"
+                            value={dailyEarning}
+                            readOnly
+                        />
+                    </FormControl>
+                </Stack>
+            </Box>
+        );
+    }
 }
 
 export default CalculatorForm;
